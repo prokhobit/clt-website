403d8c8b717e41659517b8fc59610a5e3a71c288
Alexander Prokhorov <96335939+prokhobit@users.noreply.github.com>
2026-05-24 22:35:50 -0400

    Update dustRoot selector in home.js

--- home.js
+++ home.js
@@ -24,7 +24,7 @@
     curtainPrompt: ".clt-home-curtain.is-prompt",
     curtainPanelTop: ".clt-home-curtain.is-panel-top",
 
-    dustRoot: "#star-container.clt-home-dust",
+    dustRoot: "#star-container",
     dustFar: ".clt-home-dust.is-far",
     dustMid: ".clt-home-dust.is-mid",
     dustNear: ".clt-home-dust.is-near",
